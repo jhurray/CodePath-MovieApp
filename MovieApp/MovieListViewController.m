ef436df937b66e5694ad7660621f97a718c0003c
Jeffrey Hurray <hurray@yahoo-inc.com>
2017-01-23 14:47:57 -0800

    Added network call and model for movie

--- MovieApp/MovieListViewController.m
+++ MovieApp/MovieListViewController.m
@@ -8,10 +8,12 @@
 
 #import "MovieListViewController.h"
 #import "MovieTableViewCell.h"
+#import "MovieModel.h"
 
 @interface MovieListViewController () <UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray<MovieModel *> *movies;
 
 @end
 
@@ -21,13 +23,59 @@
 {
     [super viewDidLoad];
     self.tableView.dataSource = self;
+    [self fetchMovies];
+}
+
+
+- (void)fetchMovies
+{
+    NSString *apiKey = @"a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    NSString *urlString =
+    [@"https://api.themoviedb.org/3/movie/now_playing?api_key=" stringByAppendingString:apiKey];
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    
+    NSURLSession *session =
+    [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]
+                                  delegate:nil
+                             delegateQueue:[NSOperationQueue mainQueue]];
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:^(NSData * _Nullable data,
+                                                                NSURLResponse * _Nullable response,
+                                                                NSError * _Nullable error) {
+                                                if (!error) {
+                                                    NSError *jsonError = nil;
+                                                    NSDictionary *responseDictionary =
+                                                    [NSJSONSerialization JSONObjectWithData:data
+                                                                                    options:kNilOptions
+                                                                                      error:&jsonError];
+                                                    NSLog(@"Response: %@", responseDictionary);
+                                                    
+                                                    NSArray *results = responseDictionary[@"results"];
+                                                    
+                                                    NSMutableArray *models = [NSMutableArray array];
+                                                    for (NSDictionary *result in results) {
+                                                        MovieModel *model = [[MovieModel alloc] initWithDictionary:result];
+                                                        NSLog(@"Model - %@", model);
+                                                        [models addObject:model];
+                                                    }
+                                                    self.movies = models;
+                                                    [self.tableView reloadData];
+                                                    
+                                                } else {
+                                                    NSLog(@"An error occurred: %@", error.description);
+                                                }
+                                            }];
+    [task resume];
 }
 
 
 #pragma mark - UITableViewDataSource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 20;
+    return self.movies.count;
 }
 
 
@@ -35,8 +83,9 @@
 {
     static NSString * const kCellIdentifier = @"MovieTableViewCell";
     MovieTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellIdentifier forIndexPath:indexPath];
-    cell.titleLabel.text = @"A Movie Title!";
-    cell.textView.text = @"A long description of a movie that will tell me stuff about the movie";
+    MovieModel *model = [self.movies objectAtIndex:indexPath.row];
+    cell.titleLabel.text = model.title;
+    cell.textView.text = model.movieDescription;
     cell.posterView.image = [UIImage imageNamed:@"popcorn.jpg"];
     return cell;
 }
---
+++ MovieApp/MovieModel.m
@@ -0,0 +1,26 @@
+//
+//  MovieModel.m
+//  MovieApp
+//
+//  Created by  Jeffrey Hurray on 1/23/17.
+//  Copyright © 2017 Jeffrey Hurray. All rights reserved.
+//
+
+#import "MovieModel.h"
+
+@implementation MovieModel
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    self = [super init];
+    if (self) {
+        self.title = dictionary[@"original_title"];
+        self.movieDescription = dictionary[@"overview"];
+        NSString *urlString
+        = [NSString stringWithFormat:@"https://image.tmdb.org/t/p/w45%@", dictionary[@"poster_path"]];
+        self.posterURL = [NSURL URLWithString:urlString];
+    }
+    return self;
+}
+
+@end
